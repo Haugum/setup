e6e24d0ddc459a7161b74e6d775c2d3482d59f0d
Jo Haugum <johaugum@gmail.com>
2021-04-27 23:25:35 -0400

    add app install script

---
+++ installApps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Installs a set of applications for a dev env
+
+### Install HomeBrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+### Install iTerm2 (with homebrew)
+brew install --cask iterm2
+
+### Install Firefox (with homebrew)
+brew install --cask firefox
