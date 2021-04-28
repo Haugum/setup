5f35a8a3a0e675bacf50753b63537080005cba4a
Jo Haugum <johaugum@gmail.com>
2021-04-28 18:10:10 -0400

    add homebrew

--- testInstall.sh
+++ testInstall.sh
@@ -1,3 +1,9 @@
+### Install XCode CLI tools
 sudo xcode-select --install
+
+### Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+### Install various apps
 brew install --cask $(cat caskapps.txt)
 brew install $(cat brewapps.txt)
