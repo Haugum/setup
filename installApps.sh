23ef8bde9b46f285b2ceb10724da10c59bc01f91
Jo Haugum <johaugum@gmail.com>
2021-04-27 23:40:30 -0400

    add additional apps to installApps script

--- installApps.sh
+++ installApps.sh
@@ -9,3 +9,14 @@ brew install --cask iterm2
 
 ### Install Firefox (with homebrew)
 brew install --cask firefox
+
+### Install VSCode (with homebrew)
+brew install --cask visual-studio-code
+
+### Install GoLand (with homebrew)
+brew install --cask goland
+
+### Install Spotify (with homebrew)
+brew install --cask spotify
+
+
